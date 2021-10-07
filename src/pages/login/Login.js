9c2a0297909a380a0cbc3c346d00f3e216e050f1
yukio <fabio.yamaya@gmail.com>
2021-10-06 21:49:02 -0300

    ESlint e prettier configurados

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -5,67 +5,67 @@ import api from "../../services/api";
 
 import "./Login.css";
 
-
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const history = useHistory();
+  const history = useHistory();
 
-    async function handleLogin(e){
-        e.preventDefault();
+  async function handleLogin(e) {
+    e.preventDefault();
 
-        try {
-            const response = await api.post("users/login", {
-                email,
-                password
-            });
+    try {
+      const response = await api.post("users/login", {
+        email,
+        password,
+      });
 
-            if(response.data){
-                if(response.status == 0){
-                    alert("\nE-mail incorreto!");
-                } else if(response.status == 2){
-                    alert("\nSenha incorreta!");
-                } else {
-                    history.push("/home");
-                }
-            } else {
-                alert("\nServidor indisponível!\nPor favor, tente novamente mais tarde");
-            }
-        } catch (err){
-            console.log("Erro: " + err);
-            alert("Falha no login, tente novamente.");
+      if (response.data) {
+        if (response.status == 0) {
+          alert("\nE-mail incorreto!");
+        } else if (response.status == 2) {
+          alert("\nSenha incorreta!");
+        } else {
+          history.push("/home");
         }
+      } else {
+        alert(
+          "\nServidor indisponível!\nPor favor, tente novamente mais tarde"
+        );
+      }
+    } catch (err) {
+      console.log("Erro: " + err);
+      alert("Falha no login, tente novamente.");
     }
+  }
 
-    return (
-        <div className="login-container">
-            <section className="form">
+  return (
+    <div className="login-container">
+      <section className="form">
+        <form onSubmit={handleLogin}>
+          <h1>Faça seu Login</h1>
 
-                <form onSubmit={handleLogin}>
-                    <h1>Faça seu Login</h1>
+          <input
+            type="email"
+            placeholder="Seu e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-                    <input
-                        type="email"
-                        placeholder="Seu e-mail"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+          <input
+            type="password"
+            placeholder="Sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-                    <input
-                        type="password"
-                        placeholder="Sua senha"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-
-                    <button className="button" type="submit">
-                        Entrar
-                    </button>
-                </form>
-            </section>
-        </div>
-    );
+          <button className="button" type="submit">
+            Entrar
+          </button>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
